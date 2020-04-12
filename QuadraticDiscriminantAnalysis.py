6f478d12604df7a46f426edb6e8d58881d11b36f
chetsama <chetsama@gmail.com>
2020-04-12 14:45:27 +0100

    Implemented kNN and QDA

--- QuadraticDiscriminantAnalysis.py
+++ QuadraticDiscriminantAnalysis.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 import seaborn as seabornInstance
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn import metrics
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
 
 def readCSV():
     dataSet = pd.read_csv("MultivariateInput.csv")
@@ -28,14 +30,14 @@ def main():
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-    regressor = KNeighborsRegressor()
-    regressor.fit(X_train, y_train)
+    regressorpoly3 = make_pipeline(PolynomialFeatures(3), Ridge())
+    regressorpoly3.fit(X_train, y_train)
 
     #coefficient values for each feature in attributes
-    #coeff_df = pd.DataFrame({'Coeff':regressor.coef_, "feature":selFeat})
+    #coeff_df = pd.DataFrame({'Coeff':regressorpoly2.coef_, "feature":selFeat})
     #print(coeff_df)
 
-    y_pred = regressor.predict(X_test)
+    y_pred = regressorpoly3.predict(X_test)
 
     df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
     df1 = df.head(25)
@@ -46,7 +48,7 @@ def main():
     plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
     plt.show()
 
-    print('Confidence: ', regressor.score(X_test, y_test))
+    print('Confidence: ', regressorpoly3.score(X_test, y_test))
     print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
     print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
     print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
